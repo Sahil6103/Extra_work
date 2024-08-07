0945b5b4626b733a30d8fcae82f72b268af4ff4f
sahil <sahildhandhukiya0@gmail.com>
2024-08-07 15:44:20 +0530

    now

---
+++ Tailwind CSS/Interia/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        silver: "#CAC6C0",
+        lightBrown: "#7E7366",
+        brown: "#5E452A",
+        blackBrown: "#2E2824",
+        lightBlack: "#12100E",
+        tahiti: "#3ab7bf",
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ Tailwind CSS/Tailwind Tamplate/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    screens: {
+      xsm: "320px",
+      // => @media (min-width: 320px) { ... }
+
+      sm: "520px",
+      // => @media (min-width: 520px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
